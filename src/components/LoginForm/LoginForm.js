1be257364273fea0b187eed7f01e0728e72f2c3d
Serter Iyigunlu <serteri@hotmail.com>
2023-07-12 21:32:42 +1000

    Login Form Finishing up

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,18 +1,36 @@
 import React, {useState , useEffect} from 'react'
 
-const LoginForm = ()=>{
+const LoginForm = (props)=>{
     const [enteredEmail,setEnteredEmail]= useState('')
+    const [enteredPassword,setEnteredPassword]= useState('')
+    const [formIsValid, setFormIsValid] = useState(false);
 
+    useEffect(()=>{
+       const identifier = setTimeout(()=>{
+           setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6)
+       },500)
+        return () => {clearTimeout(identifier)}
+
+    },[enteredEmail,enteredPassword]
+    )
     const emailChangeHandler= (e) => {
         setEnteredEmail(e.target.value)
     }
 
+    const passwordChangeHAndler= (e) => {
+        setEnteredPassword(e.target.value)
+    }
+const submitForm = (e) => {
+        e.preventDefault();
+
+}
     return(
         <div>
-            <form>
-                <label>Name:</label>
+            <form onSubmit={submitForm}>
+                <label>Email address:</label>
                 <input type="text" name="name" value={enteredEmail} onChange={emailChangeHandler}/>
-
+                <label>Password:</label>
+                <input type="password" name="password" value={enteredPassword} onChange={passwordChangeHAndler}/>
             </form>
         </div>
 
