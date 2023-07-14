2807c511454e70c3635818a9950a32f6ae465f18
Serter Iyigunlu <serteri@hotmail.com>
2023-07-14 22:20:15 +1000

    Home page modified

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,16 +1,25 @@
 
-
+import {Routes,Route, useNavigate} from "react-router-dom";
 import './HomePage.css';
 
 
 export function  HomePage(){
+
+    const navigate = useNavigate();
+
+    const navigatetoLogin = ()=>{
+        //navigate to /login
+        navigate('/login');
+
+    }
+
     return(
         <div>
             <div className='travel'>
                 <h2>TravelLog</h2>
                 <p>Journey Beyond Boundaries: Connect, Inspire, Explore
                 </p>
-                <button> Log In</button>
+                <button onClick={navigatetoLogin}> Log In</button>
 
             </div>
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,21 @@
-import { NavLink } from "react-router-dom"
+import {BrowserRouter, NavLink} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.css';
 
 export function  NavBar (){
+    let activeNavStyle = {
+        textDecorationColor: "green",
+        textDecorationLine: "line-through"
+    }
     return(
       <nav className='navbar navbar-expand-lg navbar-light bg-light'>
           <div className="container-fluid">
-              <NavLink className='navbar-brand ' id="navbarNav" to="/">Logo</NavLink>
+              <NavLink className='navbar-brand ' id="navbarNav" to="/" style={({isActive}) => isActive ? activeNavStyle:undefined}>Logo</NavLink>
               <div>
                   <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 
-                      <li className="nav-item "><NavLink className='nav-link active' to="/about">About</NavLink></li>
-                      <li className="nav-item "><NavLink className='nav-link' to="/projects">Log-in</NavLink></li>
-                      <li className="nav-item"><NavLink className='nav-link' to="/about/contact">Search</NavLink></li>
+                      <li className="nav-item "><NavLink className='nav-link active' to="/">Home</NavLink></li>
+                      <li className="nav-item "><NavLink className='nav-link' to="/about">About</NavLink></li>
+                      <li className="nav-item"><NavLink className='nav-link' to="/login">Login</NavLink></li>
                   </ul>
               </div>
 
