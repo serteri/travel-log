e3d6d799e59766cf3eb2a1ac9ba176f36169ddaa
djarchibald <13827@coderacademy.edu.au>
2023-07-31 13:56:25 +1000

    trying to get ahead of testing - sample test created in app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,25 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// JUST TRYING OUT TESTING
+
+// TEST FOR FOOTER()
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Footer } from './Footer'; // import the Footer component from its location
+
+describe('Footer', () => {
+  beforeEach(() => {
+    render(<Footer />);
+  });
+
+  it('should render "Copyright" text', () => {
+    expect(screen.getByText('Copyright')).toBeInTheDocument();
+  });
+
+  it('should render the current year', () => {
+    const currentYear = new Date().getFullYear().toString();
+    expect(screen.getByText(currentYear)).toBeInTheDocument();
+  });
+});
