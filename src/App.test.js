5f15fa9112eab253772201b54ad1f8d4c889c9af
djarchibald <13827@coderacademy.edu.au>
2023-08-01 13:48:23 +1000

    added test files to more components but problems with test syntax

--- src/App.test.js
+++ src/App.test.js
@@ -1,30 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
 // JUST TRYING OUT TESTING
 
-// TEST FOR FOOTER()
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Footer } from './Footer'; // import the Footer component from its location
-
-describe('Footer', () => {
-  beforeEach(() => {
-    render(<Footer />);
-  });
-
-  it('should render "Copyright" text', () => {
-    expect(screen.getByText('Copyright')).toBeInTheDocument();
-  });
-
-  it('should render the current year', () => {
-    const currentYear = new Date().getFullYear().toString();
-    expect(screen.getByText(currentYear)).toBeInTheDocument();
-  });
-});
