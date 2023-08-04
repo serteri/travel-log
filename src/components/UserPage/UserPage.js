f419a299031918bd1d81354fb540e3d2cc775dba
djarchibald <13827@coderacademy.edu.au>
2023-08-04 10:31:54 +1000

    trying to resolve source control issue

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {Routes,Route, useNavigate} from "react-router-dom";
 import './UserPage.css';
 import placeholder from '../../images/placeholderimage.png';
@@ -6,41 +5,39 @@ import placeholder from '../../images/placeholderimage.png';
 export function UserPage() {
     const navigate = useNavigate();
 
-    const createPost = ()=>{
+    const createpost = ()=>{
         //navigate to /new post page
         navigate('/login/userpage/newpost');
-    }
-
-    return(
-        <div>
-            <div className='location'>
-                <h2 className='location-title'>My Locations</h2>
-            </div>
-            <div className="location-list">
-
-                <div className="location-card">
-                    <h5>Location 1</h5>
-                </div>
-                <div className="location-card">
-                    <h5>Location 2</h5>
-                </div>
-                <div className="location-card">
-                    <h5>Location 3</h5>
-                </div>
-                <div className="location-card">
-                    <h5>Location 4</h5>
-                </div>
-                <div className="location-card">
-                    <h5>Location 5</h5>
-                </div>
-            </div>
-            <div className="new-experience">
-                <h2 className="new-experience-title" >Enter a new experience</h2>
-                <button className="button_create" onClick={createPost}> New Post</button>
-            </div>
-        </div>
-
-    )
 }
 
-module.exports = UserPage 
+return(
+    <div>
+        <div className='location'>
+            <h2 className='location-title'>My Locations</h2>    
+        </div>
+        <div className="location-list">
+
+            <div class="location-card">
+                <h5>Location 1</h5>
+            </div>
+            <div class="location-card">
+                <h5>Location 2</h5>
+            </div>
+            <div class="location-card">
+                <h5>Location 3</h5>
+            </div>
+            <div class="location-card">
+                <h5>Location 4</h5>
+            </div>
+            <div class="location-card">
+                <h5>Location 5</h5>
+            </div>
+        </div>
+        <div className="new-experience">
+            <h2 className="New-experience-title" >Enter a new experience</h2>
+            <button className="button_create" onClick={createpost}> New Post</button>
+        </div>
+    </div>
+
+    )
+}     
