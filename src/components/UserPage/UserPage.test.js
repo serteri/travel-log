d2865817bbd3dbb2523363764933458e48e35846
djarchibald <13827@coderacademy.edu.au>
2023-08-04 11:14:15 +1000

    jest test for homepage added

--- src/components/UserPage/UserPage.test.js
+++ src/components/UserPage/UserPage.test.js
@@ -1,6 +1,6 @@
-const { render, screen, fireEvent } = require('@testing-library/react');
-const { MemoryRouter, Routes, Route } = require('react-router-dom');
-const UserPage = require('./UserPage');
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { UserPage } from './UserPage';  // Change this line
 
 describe('UserPage', () => {
   it('renders correctly and navigates on button click', () => {
@@ -13,18 +13,19 @@ describe('UserPage', () => {
       </MemoryRouter>
     );
   
-      // Check that the locations and title are rendered
-      expect(getByText('My Locations')).toBeInTheDocument();
-      expect(getByText('Enter a new experience')).toBeInTheDocument();
-      expect(getByText('Location 1')).toBeInTheDocument();
-      expect(getByText('Location 2')).toBeInTheDocument();
-      // add checks for all the locations if needed
+    // Check that the locations and title are rendered
+    expect(getByText('My Locations')).toBeInTheDocument();
+    expect(getByText('Enter a new experience')).toBeInTheDocument();
+    expect(getByText('Location 1')).toBeInTheDocument();
+    expect(getByText('Location 2')).toBeInTheDocument();
+    // add checks for all the locations if needed
   
-      // Click on the "New Post" button
-      fireEvent.click(getByRole('button', { name: /new post/i }));
+    // Click on the "New Post" button
+    fireEvent.click(getByRole('button', { name: /new post/i }));
   
-      // Check that we have navigated to the new post page
-      expect(getByText('New post page')).toBeInTheDocument();
-    });
+    // Check that we have navigated to the new post page
+    expect(getByText('New post page')).toBeInTheDocument();
   });
+});
+
   
